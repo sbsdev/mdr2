6d45959fa4fc6781a392d1ca992b6b5ef788c931
Christian Egli <christian.egli@sbs.ch>
2014-06-12 10:24:01 +0200

    Add validation of the abacus import files

--- src/mdr2/abacus.clj
+++ src/mdr2/abacus.clj
@@ -1,7 +1,13 @@
 (ns mdr2.abacus
-  (:require [clj-xpath.core :refer [xml->doc $x:text]]))
+  (:require [clj-xpath.core :refer [xml->doc $x:text]])
+  (:import javax.xml.XMLConstants
+           org.xml.sax.SAXException
+           javax.xml.validation.SchemaFactory
+           javax.xml.transform.stream.StreamSource
+           java.io.File))
 
 (def root "/AbaConnectContainer/Task/Transaction/DocumentData/")
+(def schema "resources/schema/abacus_export.xsd")
 
 (def param-mapping
   {:product-number "artikel_nr"
@@ -19,3 +25,14 @@
     (into {} (for [[key path] param-mapping] 
                [key ($x:text (str root path) xml)]))))
 
+(defn valid? [file]
+  ;; basically a minimal port of
+  ;; http://stackoverflow.com/questions/15732/whats-the-best-way-to-validate-an-xml-file-against-an-xsd-file
+  (let [validator (.newValidator
+                   (.newSchema
+                    (SchemaFactory/newInstance XMLConstants/W3C_XML_SCHEMA_NS_URI)
+                    (StreamSource. (File. schema))))]
+    (try
+      (.validate validator (StreamSource. (File. file)))
+      true
+      (catch SAXException e false))))
