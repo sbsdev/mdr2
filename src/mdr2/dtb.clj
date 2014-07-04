277650e1a27a064b8cd0fc10bb63329d55ea5b02
Christian Egli <christian.egli@sbs.ch>
2014-07-04 14:11:41 +0200

    Add an alternate duration detection function
    based on the shell command `soxi`

--- src/mdr2/dtb.clj
+++ src/mdr2/dtb.clj
@@ -1,7 +1,9 @@
 (ns mdr2.dtb
   "Functions to query DAISY Talking Books"
   (:require [clojure.data.xml :as xml]
-            [clojure.java.io :refer [file]])
+            [clojure.java.io :refer [file]]
+            [clojure.java.shell :refer [sh]]
+            [clojure.string :refer [trim-newline]])
   (:import javax.sound.sampled.AudioSystem))
 
 (defn file-audio-length
@@ -15,6 +17,15 @@
         durationInSeconds (/ frames frameRate)]
     durationInSeconds))
 
+;; admitedly this is kinda hackish but the "Java-way" using
+;; AudioSystem et al doesn't seem to work inside immutant
+;; see https://issues.jboss.org/browse/IMMUTANT-457
+;; (defn file-audio-length [file]
+;;   (-> (sh "soxi" "-D" (.getPath file))
+;;       :out
+;;       trim-newline
+;;       Double/parseDouble))
+
 (defn wav-file? [file]
   (and (.isFile file)
        (.endsWith (.getName file) ".wav")))
@@ -22,6 +33,3 @@
 (defn audio-length [dtb]
   (let [audio-files (filter wav-file? (file-seq (file dtb)))]
     (reduce + (map file-audio-length audio-files))))
-
-  
-
