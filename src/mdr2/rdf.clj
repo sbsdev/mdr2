e0fc45a95940ab1bf3e9dd29c6538727a28df372
Christian Egli <christian.egli@sbs.ch>
2014-07-08 12:21:43 +0200

    Extract the fetching of the meta data into a separate function

--- src/mdr2/rdf.clj
+++ src/mdr2/rdf.clj
@@ -11,14 +11,26 @@
 (defn- page-normal [production] 0)
 (defn- page-special [production] 0)
 
+(defn get-meta-data [production]
+  (merge
+   {:depth (depth production)
+    :files (files production)
+    :size (size production)
+    :page-front (page-front production)
+    :page-normal (page-normal production)
+    :page-special (page-special production)}
+   production))
+
 (defn- rdf-sexp [production]
   (let [{:keys [title creator subject description publisher date identifier source language rights 
                 sourceDate sourceEdition sourcePublisher sourceRights sourceTitle
-                multimediaType multimediaContent narrator producedDate 
+                multimediaType multimediaContent narrator producedDate
                 revision revisionDate revisionDescription
                 totalTime audioFormat type
                 footnotes prodnotes multi-volume sidebars
-                tocItems]} production]
+                tocItems
+                depth files size
+                page-front page-normal page-special]} production]
     [:rdf:DRF {:xmlns:rdf "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
                :xmlns:dc "http://purl.org/dc/elements/1.1/"
                :xmlns:ncc "http://www.daisy.org/publications/specifications/"
@@ -36,20 +48,20 @@
       [:dc:title title]
       [:dc:type type]
       [:ncc:charset "utf-8"]
-      [:ncc:depth (depth production)]
-      [:ncc:files (files production)]
+      [:ncc:depth depth]
+      [:ncc:files files]
       (when footnotes [:ncc:footnotes footnotes])
-      [:ncc:kByteSize (size production)]
+      [:ncc:kByteSize size]
       [:ncc:multimediaType multimediaType]
       [:ncc:narrator narrator]
-      [:ncc:pageFront (page-front production)]
-      [:ncc:pageNormal (page-normal production)]
-      [:ncc:pageSpecial (page-special production)]
+      [:ncc:pageFront page-front]
+      [:ncc:pageNormal page-normal]
+      [:ncc:pageSpecial page-special]
       (when prodnotes [:ncc:prodNotes prodnotes])
       [:ncc:producedDate producedDate]
       [:ncc:revisionDate revisionDate]
       (when multi-volume
-        [:ncc:setInfo (str (:number multi-volume)) " of " (:total multi-volume)])
+        [:ncc:setInfo (str (:number multi-volume) " of " (:total multi-volume))])
       (when sidebars [:ncc:sidebars sidebars])
       [:ncc:sourceDate sourceDate]
       [:ncc:sourcePublisher sourcePublisher]
@@ -64,10 +76,11 @@
       [:sbs:kostenstelle "FIXME:"]
       [:sbs:printPageNumber "FIXME:"]]]))
 
-(defn rdf 
+(defn rdf
   "Create rdf for a given production"
   [production]
   (-> production
+      get-meta-data
       rdf-sexp
       xml/sexp-as-element
       xml/emit-str))
