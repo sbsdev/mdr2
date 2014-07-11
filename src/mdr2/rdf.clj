b79580d4b9627207e94591075dd2d0994885610a
Christian Egli <christian.egli@sbs.ch>
2014-07-11 11:47:04 +0200

    Generate an rdf according to the DAISY3 spec
    i.e. use all the attributes from
    http://www.daisy.org/z3986/2005/Z3986-2005.html. Drop all the 'ncc' and
    'sbs' attributes.
    
    Also drop the useless let and use destructuring in the argument list

--- src/mdr2/rdf.clj
+++ src/mdr2/rdf.clj
@@ -5,60 +5,47 @@
 (defn get-meta-data [{:keys [path] :as production}]
   (merge production (dtb/meta-data path)))
 
-(defn- rdf-sexp [production]
-  (let [{:keys [title creator subject description publisher date identifier source language rights 
-                sourceDate sourceEdition sourcePublisher sourceRights sourceTitle
-                multimediaType multimediaContent narrator producedDate
-                revision revisionDate revisionDescription
-                totalTime audioFormat type
-                footnotes prodnotes multi-volume sidebars
-                tocItems
-                depth files size
-                page-front page-normal page-special]} production]
-    [:rdf:DRF {:xmlns:rdf "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-               :xmlns:dc "http://purl.org/dc/elements/1.1/"
-               :xmlns:ncc "http://www.daisy.org/publications/specifications/"
-               :xmlns:sbs "http://www.sbszh.ch/sbs/namespace"}
-     ;; see also
-     ;; http://www.daisy.org/z3986/specifications/daisy_202.html?q=publications/specifications/daisy_202.html#ncc
-     [:rdf:Description
-      [:dc:creator creator]
-      [:dc:date date]
-      [:dc:format "Daisy 2.02"] ;; this is only for DAISY 2.02
-      [:dc:identifier identifier]
-      [:dc:language language]
-      [:dc:publisher publisher]
-      [:dc:source source]
-      [:dc:title title]
-      [:dc:type type]
-      [:ncc:charset "utf-8"]
-      [:ncc:depth depth]
-      [:ncc:files files]
-      (when footnotes [:ncc:footnotes footnotes])
-      [:ncc:kByteSize size]
-      [:ncc:multimediaType multimediaType]
-      [:ncc:narrator narrator]
-      [:ncc:pageFront page-front]
-      [:ncc:pageNormal page-normal]
-      [:ncc:pageSpecial page-special]
-      (when prodnotes [:ncc:prodNotes prodnotes])
-      [:ncc:producedDate producedDate]
-      [:ncc:revisionDate revisionDate]
-      (when multi-volume
-        [:ncc:setInfo (str (:number multi-volume) " of " (:total multi-volume))])
-      (when sidebars [:ncc:sidebars sidebars])
-      [:ncc:sourceDate sourceDate]
-      [:ncc:sourcePublisher sourcePublisher]
-      [:ncc:tocItems tocItems]
-      [:ncc:totalTime totalTime]
-      [:sbs:audioFormat "FIXME:"]
-      [:sbs:audioTotalTime "FIXME:"]
-      [:sbs:audioType "FIXME:"]
-      [:sbs:idMaster "FIXME:"]
-      [:sbs:idPrint "FIXME:"]
-      [:sbs:idVorstufe "FIXME:"]
-      [:sbs:kostenstelle "FIXME:"]
-      [:sbs:printPageNumber "FIXME:"]]]))
+(defn- rdf-sexp 
+  [{:keys [title creator subject description publisher date identifier source language rights 
+           sourceDate sourceEdition sourcePublisher sourceRights sourceTitle
+           multimediaType multimediaContent narrator producer producedDate
+           revision revisionDate revisionDescription
+           totalTime audioFormat type]}]
+  [:rdf:DRF {:xmlns:rdf "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+             :xmlns:dc "http://purl.org/dc/elements/1.1/"
+             :xmlns:ncc "http://www.daisy.org/publications/specifications/"
+             :xmlns:sbs "http://www.sbszh.ch/sbs/namespace"}
+   ;; see also
+   ;; http://www.daisy.org/z3986/2005/Z3986-2005.html#Over
+   [:rdf:Description
+    [:dc:title title]
+    [:dc:creator creator]
+    [:dc:subject subject]
+    [:dc:description description]
+    [:dc:publisher publisher]
+    ;; what about dc:Contributor?
+    [:dc:date date]
+    [:dc:type type]
+    [:dc:format "ANSI/NISO Z39.86-2005"] ; we are talking about DAISY3 here
+    [:dc:identifier identifier]
+    [:dc:source source]
+    [:dc:language language]
+    [:dc:rights rights]
+    [:dtb:sourceDate sourceDate]
+    [:dtb:sourceEdition sourceEdition]
+    [:dtb:sourcePublisher sourcePublisher]
+    [:dtb:sourceRights sourceRights]
+    [:dtb:sourceTitle sourceTitle]
+    [:dtb:multimediaType multimediaType]
+    [:dtb:multimediaContent multimediaContent]
+    [:dtb:narrator narrator]
+    [:dtb:producer producer]
+    [:dtb:producedDate producedDate]
+    [:dtb:revision revision]
+    [:dtb:revisionDate revisionDate]
+    [:dtb:revisionDescription revisionDescription]
+    [:dtb:totalTime totalTime]
+    [:dtb:audioFormat audioFormat]]]))
 
 (defn rdf
   "Create rdf for a given production"
