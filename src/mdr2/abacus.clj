6f0bd640a2d2e9e84bdf152b5023adb2c5d86f9e
Christian Egli <christian.egli@sbs.ch>
2014-07-15 14:28:56 +0200

    Add doc strings
    and restrict the visibility of private constants

--- src/mdr2/abacus.clj
+++ src/mdr2/abacus.clj
@@ -1,6 +1,6 @@
 (ns mdr2.abacus
   (:require [clj-xpath.core :refer [xml->doc $x:text]]
-            [clojure.java.io :as io]
+            [clojure.java.io :refer [file delete-file]]
             [immutant.messaging :as msg])
   (:import javax.xml.XMLConstants
            org.xml.sax.SAXException
@@ -8,12 +8,12 @@
            javax.xml.transform.stream.StreamSource
            java.io.File))
 
-(def root "/AbaConnectContainer/Task/Transaction/DocumentData/")
-(def schema "resources/schema/abacus_export.xsd")
+(def ^private root "/AbaConnectContainer/Task/Transaction/DocumentData/")
+(def ^private schema "resources/schema/abacus_export.xsd")
 ;;(def import-dir "/var/spool/mdr2")
-(def import-dir "/home/eglic/tmp/mdr2")
+(def ^private import-dir "/home/eglic/tmp/mdr2")
 
-(def param-mapping
+(def ^private param-mapping
   {:productNumber "artikel_nr"
    :title "MetaData/dc/title"
    :creator "MetaData/dc/creator"
@@ -24,12 +24,16 @@
    ;; :source_publisher "MetaData/sbs/verlag"
    })
 
-(defn read-file [file]
+(defn- read-file
+  "Read an export file from ABACUS and return a map with all the data"
+  [file]
   (let [xml (-> file slurp xml->doc)]
-    (into {} (for [[key path] param-mapping] 
+    (into {} (for [[key path] param-mapping]
                [key ($x:text (str root path) xml)]))))
 
-(defn valid? [file]
+(defn- valid?
+  "Check if an export file from ABACUS is valid"
+  [file]
   ;; basically a minimal port of
   ;; http://stackoverflow.com/questions/15732/whats-the-best-way-to-validate-an-xml-file-against-an-xsd-file
   (let [validator (.newValidator
@@ -41,9 +45,11 @@
       true
       (catch SAXException e false))))
 
-(defn import-file []
-  (doseq [f (filter #(.isFile %) 
-                    (file-seq (io/file import-dir)))] 
+(defn import-file
+  "Import new productions from ABACUS and put them on the create queue"
+  []
+  (doseq [f (filter #(.isFile %)
+                    (file-seq (file import-dir)))]
     (when (valid? f)
       (msg/publish "queue.create" (read-file f))
-      (io/delete-file f))))
+      (delete-file f))))
