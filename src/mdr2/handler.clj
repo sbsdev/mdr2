a5d898b660d375e546e84002ae75f48c6c0da96b
Christian Egli <christian.egli@sbs.ch>
2014-10-02 10:59:31 +0200

    Layout cosmetics

--- src/mdr2/handler.clj
+++ src/mdr2/handler.clj
@@ -16,21 +16,29 @@
   "Main routes for the application"
   (GET "/" request (views/home request))
   ;; bulk import of productions
-  (GET "/production/upload" request (friend/authenticated
-                                     (views/production-bulk-import-form request)))
-  (POST "/production/upload-confirm" [file :as r] (friend/authenticated
-                                                   (views/production-bulk-import-confirm r file)))
-  (POST "/production/upload" [productions :as r] (friend/authenticated
-                                                  (views/production-bulk-import r productions)))
+  (GET "/production/upload" request
+       (friend/authenticated (views/production-bulk-import-form request)))
+  (POST "/production/upload-confirm" [file :as r]
+        (friend/authenticated (views/production-bulk-import-confirm r file)))
+  (POST "/production/upload" [productions :as r]
+        (friend/authenticated (views/production-bulk-import r productions)))
   ;; individual productions
-  (GET "/production/:id.xml" [id] (friend/authenticated (views/production-xml id)))
-  (GET "/production/:id/upload" [id :as r] (friend/authenticated (views/file-upload-form r id)))
-  (POST "/production/:id/upload" [id file :as r] (friend/authenticated (views/production-add-xml r id file)))
-  (POST "/production/:id/state" [id state :as r] (friend/authenticated (views/production-set-state r id state)))
-  (GET "/catalog" request (friend/authenticated (views/catalog request)))
-  (POST "/catalog/:id" [id library_signature :as r] (friend/authenticated (views/production-catalog r id library_signature)))
-  (GET "/production/:id" [id :as r] (friend/authenticated (views/production r id)))
-  (GET "/production/:id/delete" [id] (friend/authorize #{:admin} (views/production-delete id)))
+  (GET "/production/:id.xml" [id]
+       (friend/authenticated (views/production-xml id)))
+  (GET "/production/:id/upload" [id :as r]
+       (friend/authenticated (views/file-upload-form r id)))
+  (POST "/production/:id/upload" [id file :as r]
+        (friend/authenticated (views/production-add-xml r id file)))
+  (POST "/production/:id/state" [id state :as r]
+        (friend/authenticated (views/production-set-state r id state)))
+  (GET "/catalog" request
+       (friend/authenticated (views/catalog request)))
+  (POST "/catalog/:id" [id library_signature :as r]
+        (friend/authenticated (views/production-catalog r id library_signature)))
+  (GET "/production/:id" [id :as r]
+       (friend/authenticated (views/production r id)))
+  (GET "/production/:id/delete" [id]
+       (friend/authorize #{:admin} (views/production-delete id)))
   ;; auth
   (GET "/login" [] (views/login-form))
   (GET "/logout" req (friend/logout* (response/redirect "/")))
@@ -41,7 +49,7 @@
 (def app
   "Main handler for the application"
   (-> app-routes
-      (friend/authenticate 
+      (friend/authenticate
        {:credential-fn (partial creds/bcrypt-credential-fn db/get-user)
         :workflows [(workflows/interactive-form)]
         :unauthorized-handler views/unauthorized})
