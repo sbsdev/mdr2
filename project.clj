70b3a5cb96079f72a447e74cf485b8421e7787c9
Christian Egli <christian.egli@sbs.ch>
2014-12-01 15:59:35 +0100

    Upgrade dependencies

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "https://github.com/sbsdev/mdr2"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [hiccup "1.0.5"]
-                 [compojure "1.2.1"]
+                 [compojure "1.2.2"]
                  [org.clojure/java.jdbc "0.3.6"]
-                 [mysql/mysql-connector-java "5.1.25"]
+                 [mysql/mysql-connector-java "5.1.34"]
                  [org.clojure/data.xml "0.0.8"]
                  [org.clojure/data.zip "0.1.1"]
                  [org.clojure/tools.logging "0.3.1"]
@@ -17,13 +17,13 @@
                  [ring/ring-defaults "0.1.2"]
                  [com.cemerick/friend "0.2.1"]
                  [me.raynes/fs "1.4.6"] ; to be replaced by nio.file
-                 [org.tobereplaced/nio.file "0.2.0"]
+                 [org.tobereplaced/nio.file "0.3.0"]
                  [org.clojure/data.csv "0.1.2"]
                  [com.novemberain/pantomime "2.3.0"]
                  [clj-time "0.8.0"]
                  [environ "1.0.0"]
-                 [org.immutant/immutant "2.0.0-alpha2"]
-                 [ring/ring-devel "1.3.1"]]
+                 [org.immutant/immutant "2.0.0-beta1"]
+                 [ring/ring-devel "1.3.2"]]
   :repositories [["Immutant 2.x incremental builds"
                   "http://downloads.immutant.org/incremental/"]]
   :plugins [[lein-immutant "2.0.0-alpha2"]
@@ -38,6 +38,5 @@
   :main mdr2.main
   :immutant {:war {:context-path "/"}}
   :profiles {:dev {:dependencies [[org.xerial/sqlite-jdbc "3.8.7"]
-                                  [javax.servlet/servlet-api "2.5"]
-                                  [ring/ring-devel "1.3.1"]]}
+                                  [javax.servlet/servlet-api "2.5"]]}
              :test {:dependencies [[ring-mock "0.1.5"]]}})
