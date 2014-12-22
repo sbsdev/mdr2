c5711503b510e3e0885ab21d7c86bed5da8b33df
Christian Egli <christian.egli@sbs.ch>
2014-12-22 11:51:14 +0100

    Enhance the logging instrumentation

--- dev/user.clj
+++ dev/user.clj
@@ -1,16 +1,41 @@
 (ns user
   (:require [robert.hooke :as rh]
-            [clojure.tools.logging :as log]
-            [mdr2.db :as db]))
+            [clojure.tools.logging :as log]))
 
-(defn debug [f & args]
-  (log/debug "Calling " (:name (meta f)) " with " args)
+;; inspiration from
+;; http://martintrojer.github.io/clojure/2014/03/09/working-with-coreasync-blocking-calls/
+
+(defn debug [{:keys [name ns]} f & args]
+  (log/debug "Calling " (ns-name ns) "/" name " with " args)
   (apply f args))
 
+(defn enable-debug [var]
+  (log/debug "Enabling debugging for" var)
+  (robert.hooke/add-hook var (partial debug (meta var))))
+
+(defn disable-debug [var]
+  (log/debug "Disabling debugging for" var)
+  (robert.hooke/clear-hooks var))
+
+(defn public-fns [sym]
+  (->> sym
+   find-ns
+   ns-publics
+   vals
+   (filter #(fn? (deref %)))))
+
 (defn debug-ns [sym]
-  (doseq [v (vals (ns-publics (find-ns sym)))]
-    (rh/add-hook v #'debug))
-  )
+  (->> (public-fns sym)
+   ;; remove dups
+   (remove #(:robert.hooke/hooks (meta (deref %)))) ; this doesn't seem to work
+   (map enable-debug)
+   doall))
+
+(defn undebug-ns [sym]
+  (->> (public-fns sym)
+   ;; (filter #(:robert.hooke/hook (meta (deref %))))
+   (map disable-debug)
+   doall))
 
 (comment
   (debug-ns 'mdr2.db))
