c9d6f0950f3a8ff37273a5b248dc97c4924c5bac
Christian Egli <christian.egli@sbs.ch>
2015-01-21 15:02:28 +0100

    Add test cases for single and multivolume books

--- test/mdr2/test/archive.clj
+++ test/mdr2/test/archive.clj
@@ -5,15 +5,16 @@
             [clojure.java.io :refer [file]]
             [environ.core :refer [env]]))
 
-(def mock-db (atom ()))
+(def mock-db (atom []))
 
 (defn mock-jdbc-insert [db table job]
-  (reset! mock-db job))
+  (swap! mock-db conj job))
 
 (defn setup-spool-dirs [test-fn]
   (fs/mkdirs (env :archive-spool-dir))
   (fs/mkdirs (env :archive-periodical-spool-dir))
   (fs/mkdirs (env :archive-other-spool-dir))
+  (reset! mock-db [])
   (with-redefs [clojure.java.jdbc/insert! mock-jdbc-insert]
     (test-fn))
   (fs/delete-dir (env :archive-spool-dir))
@@ -29,37 +30,109 @@
 
   (testing "container path"
     (is (= (#'mdr2.archive/container-path {:id 123} :master)
-           (.getPath (file (env :archive-spool-dir) "dam123")))))
+           (.getPath (file (env :archive-spool-dir) "dam123" "produkt")))))
 
   (testing "rdf path"
     (is (= (#'mdr2.archive/container-rdf-path {:id 123} :master)
-           (.getPath (file (env :archive-spool-dir) "dam123" "dam123.rdf")))))
+           (.getPath (file (env :archive-spool-dir) "dam123" "dam123.rdf"))))))
 
+(deftest add-master-to-db
   (testing "add master to db"
     (is (= (do
              (#'mdr2.archive/add-to-db {:id 123 :volumes 1} :master)
              @mock-db)
-           {:verzeichnis "dam123",
-            :sektion "master",
-            :aktion "save",
-            :transaktions_status "pending",
-            :abholer "NN",
-            :archivar "NN",
-            :flags "x"})))
+           [{:verzeichnis "dam123",
+             :sektion "master",
+             :aktion "save",
+             :transaktions_status "pending",
+             :abholer "NN",
+             :archivar "NN",
+             :flags "x"}]))))
 
+(deftest add-iso-to-db
   (testing "add iso to db"
     (is (= (do
              (#'mdr2.archive/add-to-db
               {:id 123 :volumes 1 :library_signature 7}
               :dist-master)
              @mock-db)
-           {:verzeichnis "ds7",
-            :sektion "cdimage",
-            :aktion "save",
-            :transaktions_status "pending",
-            :abholer "NN",
-            :archivar "NN",
-            :flags "x"}))))
+           [{:verzeichnis "ds7",
+             :sektion "cdimage",
+             :aktion "save",
+             :transaktions_status "pending",
+             :abholer "NN",
+             :archivar "NN",
+             :flags "x"}]))))
+
+(deftest book-single
+  (testing "archiving a book with a single volume"
+    (let [production {:id 14 :production_type "book" :volumes 1 :library_signature 15}
+          dam-number (str "dam" (:id production))
+          ds-number (str "ds" (:library_signature production))
+          iso-name (str ds-number ".iso")
+          spool-dir (env :archive-spool-dir)]
+      (archive/archive production)
+      ;; check the database entry
+      (is (= @mock-db [{:sektion "master"
+                        :aktion "save"
+                        :transaktions_status "pending"
+                        :abholer "NN"
+                        :archivar "NN"
+                        :verzeichnis "dam14"
+                        :flags "x"}
+                       {:verzeichnis "ds15"
+                        :sektion "cdimage"
+                        :aktion "save"
+                        :transaktions_status "pending"
+                        :abholer "NN"
+                        :archivar "NN"
+                        :flags "x"}]))
+      (are [file] (fs/exists? file)
+           ;; check if the master has been copied
+           (fs/file spool-dir dam-number "produkt" "aud001.wav")
+           (fs/file spool-dir dam-number "produkt" "ncc.html")
+           (fs/file spool-dir dam-number "produkt" "master.smil")
+           ;; check if the master has an rdf
+           (fs/file spool-dir dam-number (str dam-number ".rdf"))
+           ;; check if the distmaster has been copied...
+           (fs/file spool-dir ds-number "produkt" iso-name)
+           ;; ...and that it has an rdf
+           (fs/file spool-dir ds-number (str ds-number ".rdf"))))))
+
+(deftest book-multiple
+  (testing "archiving a book with multiple volumes"
+    (let [production {:id 14 :production_type "book" :volumes 2 :library_signature 15}
+          dam-number (str "dam" (:id production))
+          ds-number (str "ds" (:library_signature production))
+          spool-dir (env :archive-spool-dir)]
+      (archive/archive production)
+      ;; check the database entry
+      (is (= @mock-db [{:sektion "master"
+                        :aktion "save"
+                        :transaktions_status "pending"
+                        :abholer "NN"
+                        :archivar "NN"
+                        :verzeichnis "dam14"
+                        :flags "x"}
+                       {:verzeichnis "ds15"
+                        :sektion "cdimage"
+                        :aktion "save"
+                        :transaktions_status "pending"
+                        :abholer "NN"
+                        :archivar "NN"
+                        :flags "x"}]))
+      (are [file] (fs/exists? file)
+           ;; check if the master has been copied
+           (fs/file spool-dir dam-number "produkt" "aud001.wav")
+           (fs/file spool-dir dam-number "produkt" "ncc.html")
+           (fs/file spool-dir dam-number "produkt" "master.smil")
+           ;; check if the master has an rdf
+           (fs/file spool-dir dam-number (str dam-number ".rdf"))
+           ;; check if all the distmasters have been copied...
+           (fs/file spool-dir ds-number "produkt" (str ds-number "_1.iso"))
+           (fs/file spool-dir ds-number "produkt" (str ds-number "_2.iso"))
+           ;; ...and that it has an rdf
+           (fs/file spool-dir ds-number (str ds-number ".rdf"))))))
 
 (deftest periodicals-single
   (testing "archiving of periodical single volume"
