e78067cf7af1c18be5db28e2c403226f992d3022
Christian Egli <christian.egli@sbs.ch>
2015-01-27 16:42:54 +0100

    Remove unneeded queries

--- src/mdr2/archive/queries.sql
+++ src/mdr2/archive/queries.sql
@@ -3,13 +3,3 @@
 SELECT id
 FROM container
 WHERE verzeichnis = :id
-
--- name: library-signature-to-archive-id
--- Return an archive id for a given library signature, i.e. ds number
-SELECT id
-FROM container
-WHERE verzeichnis = (
-      SELECT LOWER(REPLACE(value,' ', '')) FROM meta
-      WHERE ns='sbs' AND element = 'idMaster' AND id_container IN
-      	    (SELECT id FROM container WHERE verzeichnis = :signature))
-
