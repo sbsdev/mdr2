d31caa8800e126a1753efdf174febfed0c54a511
Christian Egli <christian.egli@sbs.ch>
2015-02-19 11:54:02 +0100

    Only pass the produced_date to ABACUS if non-nil

--- src/mdr2/abacus.clj
+++ src/mdr2/abacus.clj
@@ -145,7 +145,8 @@
         (create-row 280 audio_format)
         (create-row 271 volumes) ; Number of CDs
         (create-row 281 multimedia_type)
-        (create-row 256 (format date-format produced_date)) ; Date of production end
+        (create-row 256 (and produced_date
+                             (format date-format produced_date))) ; Date of production end
         (create-row 278 (format date-format date)) ; Date of production start
         (create-row 4 (prod/dam-number production))] ; for legacy purposes
        (remove nil?) ; remove empty rows
