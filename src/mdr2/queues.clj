3d92368f6b2284a4dfe0b37d46e26005019b19c8
Christian Egli <christian.egli@sbs.ch>
2015-03-24 14:24:53 +0100

    remove unused queues

--- src/mdr2/queues.clj
+++ src/mdr2/queues.clj
@@ -1,10 +1,7 @@
 (ns mdr2.queues
   (:require [immutant.messaging :as msg]))
 
-(defn create [] (msg/queue "create"))
 (defn encode [] (msg/queue "encode"))
-(defn encode-multi-volume [] (msg/queue "encode-multi-volume"))
 (defn archive [] (msg/queue "archive"))
 (defn notify-abacus [] (msg/queue "notify-abacus"))
-(defn metadata-update [] (msg/queue "metadata-update"))
 
