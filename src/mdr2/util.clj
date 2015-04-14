4f0efe142b96b709465b2ad964370e6a61200474
Christian Egli <christian.egli@sbs.ch>
2015-04-14 12:23:27 +0200

    Catch FileSystemException

--- src/mdr2/util.clj
+++ src/mdr2/util.clj
@@ -1,13 +1,19 @@
 (ns mdr2.util
   "Common utilies"
-  (:require [org.tobereplaced.nio.file :as nio]))
+  (:require [org.tobereplaced.nio.file :as nio]
+            [clojure.tools.logging :as log])
+  (:import java.nio.file.FileSystemException))
 
 (defn delete-directory!
   "Delete `directory` recursively. See
   https://github.com/ToBeReplaced/nio.file/blob/master/walkthrough.clj"
   [directory]
-  (nio/walk-file-tree directory
-   (nio/naive-visitor
-    :post-visit-directory nio/delete!
-    :visit-file nio/delete!)))
+  (try
+    (nio/walk-file-tree
+     directory
+     (nio/naive-visitor
+      :post-visit-directory nio/delete!
+      :visit-file nio/delete!))
+    (catch FileSystemException e
+      (log/error e))))
 
