5104ef360e4551c972bf2689dc224cbc265ba145
Christian Egli <christian.egli@sbs.ch>
2016-11-07 12:13:17 +0100

    Define the Emacs compile command

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil
+  (compile-command . "lein immutant war --nrepl-start --nrepl-port 42278")))
