edd2ac84f3525d59cbbda8c22618f66fae873e4c
Christian Egli <christian.egli@sbs.ch>
2022-06-23 13:22:09 +0200

    Define the type of library_signature more strictly

--- src/clj/mdr2/production/spec.clj
+++ src/clj/mdr2/production/spec.clj
@@ -2,10 +2,9 @@
   (:require [spec-tools.data-spec :as spec]
             [clojure.spec.alpha :as s]))
 
-(s/def ::state (s/and string? #{"new" "archived" "cataloged"
-                                "deleted" "encoded" "failed"
-                                "pending-split" "recorded" "split" "structured"}))
+(s/def ::state (s/and string? #{"new" "archived" "cataloged" "deleted" "encoded" "failed" "pending-split" "recorded" "split" "structured"}))
 (s/def ::production_type (s/and string? #{"book" "periodical" "other"}))
+(s/def ::library_signature (s/and string? #(re-matches #"^ds\d{5,}$" %)))
 (s/def ::date #(instance? java.time.LocalDate %))
 
 (def production
@@ -43,6 +42,6 @@
    (spec/opt :production_type) ::production_type
    (spec/opt :periodical_number) string?
    (spec/opt :library_number) string?
-   (spec/opt :library_signature) string?
+   (spec/opt :library_signature) ::library_signature
    (spec/opt :library_record_id) int?
    })
