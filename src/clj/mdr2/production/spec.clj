a9af30fbc3193529e451f2692fa39b47bf285c46
Christian Egli <christian.egli@sbs.ch>
2022-11-22 15:00:50 +0100

    Make coercion work for local dates
    Thanks to a gist by Tommi Reiman at
    https://gist.github.com/ikitommi/a64314c915cffe46fbd2cef95074a12f
    
    Thanks to this change the API for creating products now also works.

--- src/clj/mdr2/production/spec.clj
+++ src/clj/mdr2/production/spec.clj
@@ -1,6 +1,24 @@
 (ns mdr2.production.spec
   (:require [spec-tools.data-spec :as spec]
+            [spec-tools.core :as st]
             [clojure.spec.alpha :as s]))
+
+;; see https://gist.github.com/ikitommi/a64314c915cffe46fbd2cef95074a12f
+;; a spec-tools transformer
+(defn- decode-local-date [_ value]
+  (try
+    (if (string? value)
+      (java.time.LocalDate/parse value))
+    (catch Exception _
+      value)))
+
+;; a local-date spec
+(s/def ::local-date
+  (st/spec
+    {:spec (partial instance? java.time.LocalDate)
+     :json-schema/type {:type "string", :format "date"}
+     :decode/string decode-local-date
+     :decode/json decode-local-date}))
 
 (s/def ::state (s/and string?
                       #{"new" "archived" "cataloged" "deleted" "encoded"
@@ -9,7 +27,6 @@
 (s/def ::production_type (s/and string? #{"book" "periodical" "other"}))
 (s/def ::library_signature (s/and string? #(re-matches #"^ds\d{4,}$" %)))
 (s/def ::library_number (s/and string? #(re-matches #"^PNX \d{4,}$" %)))
-(s/def ::date #(instance? java.time.LocalDate %))
 
 (s/def ::volumes (s/and int? #{1 2 3 4 5 6 7 8}))
 (s/def ::sample-rate (s/and int? #{11025 22050 44100 48000}))
@@ -21,7 +38,7 @@
    (spec/opt :subject) string?
    (spec/opt :description) string?
    (spec/opt :publisher) string?
-   :date ::date
+   :date ::local-date
    (spec/opt :type) string?
    (spec/opt :format) string?
    (spec/opt :id) int?
@@ -29,7 +46,7 @@
    (spec/opt :source) string?
    :language string?
    (spec/opt :rights) string?
-   (spec/opt :source_date) ::date
+   (spec/opt :source_date) ::local-date
    (spec/opt :source_edition) string?
    (spec/opt :source_publisher) string?
    (spec/opt :source_rights) string?
@@ -37,9 +54,9 @@
    (spec/opt :multimedia_content) string?
    (spec/opt :narrator) string?
    (spec/opt :producer) string?
-   (spec/opt :produced_date) ::date
+   (spec/opt :produced_date) ::local-date
    :revision int?
-   (spec/opt :revision_date) ::date
+   (spec/opt :revision_date) ::local-date
    (spec/opt :revision_description) string?
    (spec/opt :total_time) int?
    (spec/opt :audio_format) string?
