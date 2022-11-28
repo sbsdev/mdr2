988ef41f3a016f490856e8362c9e928b97d86ed5
Christian Egli <christian.egli@sbs.ch>
2022-11-28 11:16:05 +0100

    Increase the queue buffer size to 50

--- src/clj/mdr2/queues.clj
+++ src/clj/mdr2/queues.clj
@@ -5,24 +5,24 @@
 
 (defstate encode
   "Queue to asynchronously handle events to encode a production"
-  :start (chan (buffer 30))
+  :start (chan (buffer 50))
 
   :stop (when encode (close! encode)))
 
 (defstate archive
   "Queue to asynchronously handle events to archive a production"
-  :start (chan (buffer 30))
+  :start (chan (buffer 50))
   
   :stop (when archive (close! archive)))
 
 (defstate repair
   "Queue to asynchronously handle events to repair a production"
-  :start (chan (buffer 30))
+  :start (chan (buffer 50))
 
   :stop (when repair (close! repair)))
 
 (defstate notify-abacus
   "Queue to asynchronously handle events to notify ABACUS of state changes in a production"
-  :start (chan (buffer 30))
+  :start (chan (buffer 50))
   
   :stop (when notify-abacus (close! notify-abacus)))
