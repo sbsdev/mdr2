1b8abd0d3ee129b9c89f76efa35d4ec40c57188f
Christian Egli <christian.egli@sbs.ch>
2022-12-22 10:41:21 +0100

    Increase the queue size for encoding and archiving to 100
    especially archiving can happen in bursts and also takes quite some
    time, so it makes sense to have a longer queue. Otherwise we might get
    a timeout when trying to archive more books once the queue is full.

--- src/clj/mdr2/queues.clj
+++ src/clj/mdr2/queues.clj
@@ -5,14 +5,17 @@
 
 (defstate encode
   "Queue to asynchronously handle events to encode a production"
-  :start (chan (buffer 50))
+  ;; encoding takes a while, so we need a decent buffer size
+  :start (chan (buffer 100))
 
   :stop (when encode (close! encode)))
 
 (defstate archive
   "Queue to asynchronously handle events to archive a production"
-  :start (chan (buffer 50))
-  
+  ;; archiving takes a while and happens in bursts, so we need a
+  ;; decent buffer size
+  :start (chan (buffer 100))
+
   :stop (when archive (close! archive)))
 
 (defstate repair
@@ -24,5 +27,5 @@
 (defstate notify-abacus
   "Queue to asynchronously handle events to notify ABACUS of state changes in a production"
   :start (chan (buffer 50))
-  
+
   :stop (when notify-abacus (close! notify-abacus)))
