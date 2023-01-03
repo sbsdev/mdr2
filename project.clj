bf577bb6712019b6d2393a9ff40d545d5693adc9
Christian Egli <christian.egli@sbs.ch>
2023-01-03 11:54:16 +0100

    Version 0.9.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ch.sbs/mdr2 "0.9.2"
+(defproject ch.sbs/mdr2 "0.9.3-SNAPSHOT"
 
   :description "A Production Management Tool for DAISY Talking Books"
   :url "https://github.com/sbsdev/mdr2"
