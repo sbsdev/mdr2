c3072dab2be8c9670281ac8a4a4b8d21eec2c271
Christian Egli <christian.egli@sbs.ch>
2023-06-14 16:31:07 +0200

    Make a require alias more clear

--- src/clj/mdr2/pipeline2/scripts.clj
+++ src/clj/mdr2/pipeline2/scripts.clj
@@ -4,16 +4,16 @@
   invoke specific scripts."
   (:require
    [babashka.fs :as fs]
-   [mdr2.pipeline2.core :as dp2]))
+   [mdr2.pipeline2.core :as pipeline2]))
 
 (defn validate [input & {:keys [mathml-version check-images] :as opts}]
-  (dp2/create-job-and-wait "dtbook-validator" {} (merge opts {:input-dtbook input})))
+  (pipeline2/create-job-and-wait "dtbook-validator" {} (merge opts {:input-dtbook input})))
 
 (defn daisy3-to-epub3 [input & {:keys [mediaoverlays assert-valid] :as opts}]
-  (dp2/create-job-and-wait "daisy3-to-epub3" {:source input} opts))
+  (pipeline2/create-job-and-wait "daisy3-to-epub3" {:source input} opts))
 
 (defn epub3-to-daisy202 [input & {:keys [temp-dir output-dir] :as opts}]
-  (dp2/create-job-and-wait "epub3-to-daisy202" {} (merge {:epub input} opts)))
+  (pipeline2/create-job-and-wait "epub3-to-daisy202" {} (merge {:epub input} opts)))
 
 (defn daisy3-to-daisy202 [input output-dir]
   (let [epub-dir (fs/create-temp-dir "mdr2")]
